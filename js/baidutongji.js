60d342e52c112288a52a6b8016dcfe77f3d008e4
HelloWorld <104149332+ming-14@users.noreply.github.com>
2022-05-08 20:38:15 +0800

    Update baidutongji.js

--- js/baidutongji.js
+++ js/baidutongji.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?b6b5c4ce1b084ea245d17d5879f532e9";
+  hm.src = "https://hm.baidu.com/hm.js?66abdce367acbbebf8318416dc7a2cc9";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();

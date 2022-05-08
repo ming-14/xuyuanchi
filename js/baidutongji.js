a7ac6e32fff3f6ba840239e028b101ae63ed26e5
HelloWorld <104149332+ming-14@users.noreply.github.com>
2022-05-08 19:22:44 +0800

    Add files via upload

---
+++ js/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b6b5c4ce1b084ea245d17d5879f532e9";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();

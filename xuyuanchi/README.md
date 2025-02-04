be830ed65628780f5823c8c731ad4c4fc3ef3ca5
HelloWorld <104149332+ming-14@users.noreply.github.com>
2025-02-04 18:04:07 +0800

    Add files via upload

---
+++ xuyuanchi/README.md
@@ -0,0 +1,53 @@
+我还是那么爱NodeJS~~
+
+---
+效果 <https://ming-14.github.io/xuyuanchi-pages/>
+
+---
+## 项目介绍
+一个simple的许愿池
+
+## 目录结构
+```
+xuyuanchi
+│  README.md README
+│
+├─public 前端界面
+│  │  favicon.ico
+│  │  index.html
+│  │
+│  ├─css CSS
+│  │      main.css
+│  │
+│  └─js JavaScript
+│      │  main.js
+│      │  tool.js
+│      │
+│      └─library
+│          │  cookie.min.js Cookie操作
+│          │
+│          ├─mouse-click
+│          │      mouse-click-love.min.js 鼠标点击产生爱心
+│          │
+│          └─snow 网站下雪
+│                  snowy.min.css
+│                  snowy.min.js
+│
+└─serivce 后端API
+        config.js 配置文件，包括数据库信息
+        database.js 数据库连接模块
+        package-lock.json 
+        package.json
+        serivce.js 主程序
+```
+
+## 如何使用
+./serivce/config.js 填写数据库信息  
+./public/js/main.js 填写变量 `api_url`，如 `var api_url = "https://example.com";`，加协议头不加末尾斜杠  
+在后端运行 `npm install && node serivce.js`，默认开启端口为`3000`  
+访问 `后台地址/install`  
+最后把 `public/*` 放在你的服务器就OK啦
+
+## 缺陷 And TODO
+[X] 后台管理员界面  
+所以只能是一个MySQL增删查~~改~~的基本Project啦！
---
+++ xuyuanchi/serivce/serivce.js
@@ -0,0 +1,124 @@
+const database = require("./database");
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+// 连接数据库
+const db = database.connectDatabase();
+
+function isAdmin(account, password)
+{
+    return (account === config.AdminAccount && password === config.AdminPassword);
+}
+
+app.all("*", function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // Your website address
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type, Authorization"
+    );
+    res.setHeader("Content-Type", "application/json");
+    next();
+});
+
+// +-+-+-+-+-+-+-+-
+// 初始化数据库
+// 表：id, content, date
+// +-+-+-+-+-+-+-+-
+app.get('/install', (req, res) => {
+    let boxsSQL = `
+    CREATE TABLE IF NOT EXISTS boxs (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    content text NOT NULL,
+    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)`;
+    db.query(boxsSQL, (err, result) => {
+        if (err) throw err;
+        else console.log('boxs表插入成功');
+    });
+
+    res.json({ code: 200 });
+});
+
+// +-+-+-+-+-+-+-+-
+// 获取所有box
+// 返回
+//[
+//    {
+//        "id": 1,
+//          "content": "t",
+//          "date": "2025-01-21T12:37:02.000Z"
+//      }
+//]
+// +-+-+-+-+-+-+-+-
+app.get('/api/getBoxs', (req, res) => {
+    let sql = "SELECT * FROM boxs";
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.json({ code: -1 });
+        } else res.json(result);
+    });
+});
+
+// +-+-+-+-+-+-+-+-
+// 获取一条box
+// /api/getABox/:id
+// +-+-+-+-+-+-+-+-
+app.get('/api/getABox/:id', (req, res) => {
+    let sql = `SELECT * FROM boxs WHERE id = ${req.params.id}`;
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.json({ code: -1 });
+        } else res.json(result);
+    });
+});
+
+// +-+-+-+-+-+-+-+-
+// 删除一条box
+// /api/deleteABox/:id
+// req：
+// {
+//   account: "",
+//   password: ""
+// }
+// +-+-+-+-+-+-+-+-
+app.post('/api/deleteABox/:id', (req, res) => {
+    if(!isAdmin(req.params.account, req.params.password)) 
+    {
+        res.json({ code: 403 });
+        return;
+    }
+    let sql = `DELETE FROM boxs WHERE id = ${req.params.id}`;
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.json({ code: -1 });
+        } else res.json({ code: 200 });
+    });
+});
+
+// +-+-+-+-+-+-+-+-
+// 接受POST请求
+// req：
+// {
+//   data: "数据"
+// }
+// +-+-+-+-+-+-+-+-
+app.post('/api/post', (req, res) => {
+    let sql = `INSERT INTO boxs (content) VALUES (?)`;
+    db.query(sql, [req.body.data], (err, result) => {
+        if (err) {
+            console.error(err);
+            res.json({ code: -1 });
+        } else {
+            console.log('数据：', req.body.data, " 成功写入数据库");
+            res.json({ code: 200 });
+        }
+    });
+});
+
+app.listen(config.port, () => {
+    console.log(`服务器已打开, 可以运行 http://127.0.0.1:${config.port}`);
+});
